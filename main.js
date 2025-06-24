6afb8d3a633d91c7842c7b7f7ab59f9056d60a51
chlodwig1 <homebudget@protonmail.com>
2025-06-24 22:21:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const express = require("express");
-const url = required("url");
+const url = require("url");
 const app = express();
 
 app.use(express.static("public"));
