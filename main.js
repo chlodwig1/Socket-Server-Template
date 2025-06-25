e7f8a8d2d14e7cb9e851c0e8e675898a70e53a2e
chlodwig1 <homebudget@protonmail.com>
2025-06-25 16:01:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ server.listen(serverPort);
 console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
 
 wss.on("connection", function (ws, req) {
-  console.log("Connection Opened");
+  console.log("Connection Opened!");
   console.log("Client size: ", wss.clients.size);
 
   const queryParams = url.parse(req.url, true).query;
@@ -34,20 +34,33 @@ wss.on("connection", function (ws, req) {
   const clubId = queryParams.clubId;
   const tournamentId = queryParams.tournamentId;
   const employeesOnly = !!queryParams.employeesOnly;
-  console.log("query params: ", clientId, clubId, tournamentId, employeesOnly);
-
-/*
- query params:  81399 365214 undefined true
-  query params:  87869 365214 1000132 false
-*/
 
   if (clientId !== undefined && clubId !== undefined) {
     if (tournamentId === undefined) {
-      tournamentsOfClubMap.set(clubId, ws);
+      tournamentsOfClubMap.set(
+        clientId, 
+        {
+          ws: ws,
+          clubId: clubId
+        }
+      );
     } else if (employeesOnly === true) {
-      employeesOnlyMap.set(clubId, ws);
+      employeesOnlyMap.set(
+        clientId, 
+        {
+          ws: ws,
+          clubId: clubId
+        } 
+      );
     } else {
-      tournamentMap.set(tournamentId, ws);
+      tournamentMap.set(
+        clientId, 
+        {
+          ws: ws,
+          tournamentId: tournamentId,
+          clubId: clubId
+        }
+      );
     }
   }
   
@@ -58,15 +71,16 @@ wss.on("connection", function (ws, req) {
   }
 
   ws.on("message", (data) => {
-    let stringifiedData = data.toString();
-    if (stringifiedData === 'pong') {
+    const stringifiedData = data.toString();
+    const parsedData = JSON.parse(stringifiedData);
+    const scope = parsedData.scope;
+
+    if (stringifiedData === 'pong' || scope === 'HEARTBEAT') {
       console.log('keepAlive');
       return;
     }
-
-    console.log(stringifiedData);
     
-    broadcast(ws, stringifiedData);
+    broadcast(ws, stringifiedData, parsedData);
   });
 
   ws.on("close", (data) => {
@@ -80,27 +94,121 @@ wss.on("connection", function (ws, req) {
 });
 
 // Implement broadcast function because of ws doesn't have it
-const broadcast = (ws, message) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+const broadcast = (ws, message, data) => {
+    const clientId = data.clientId;
+      removeAllClosedClients();
+  
+    if (data.scope === 'TOURNAMENT') {
+      console.log('BROADCASTING TO TOURNAMENT', data.tournamentId, 'in club', data.clubId);
+      [...tournamentMap]
+        .filter(([key, _]) => key !== clientId)
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+          console.log('check client', pair.cId);
+
+          console.log(pair.clientConfig.ws.readyState, WebSocket.CLOSED)
+
+          const shouldBroadcast = pair.clientConfig.ws !== ws 
+          && pair.clientConfig.ws.readyState === WebSocket.OPEN
+          && +data.clubId === +pair.clientConfig.clubId
+          && +data.tournamentId === +pair.clientConfig.tournamentId
+        
+
+          console.log('should broadcast', shouldBroadcast);
+
+          if (shouldBroadcast) {
+            console.log('==> SENDING TO CLIENT', pair.cId)
+            pair.clientConfig.ws.send(message);
+          }
+        });
+        
+      console.log('BROADCASTING TO ALL TOURNAMENTS IN CLUB', data.clubId);
+      [...tournamentsOfClubMap]
+        .filter(([key, _]) => key !== clientId)
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+          console.log('check client', pair.cId);
+
+          console.log(pair.clientConfig.ws.readyState, WebSocket.CLOSED)
+
+          const shouldBroadcast = pair.clientConfig.ws !== ws 
+          && pair.clientConfig.ws.readyState === WebSocket.OPEN
+          && +data.clubId === +pair.clientConfig.clubId
+
+          console.log('should broadcast', shouldBroadcast);
+
+          if (shouldBroadcast) {
+            pair.clientConfig.ws.send(message);
+          }
+        });
+    }
+
+    if (data.scope === 'EMPLOYEES') {
+      console.log('BROADCASTING TO ALL EMPLOYEES IN CLUB', data.clubId);
+      [...employeesOnlyMap]
+        .filter(([key, _]) => key !== clientId)
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+          console.log('check client', pair.cId);
+
+          console.log(pair.clientConfig.ws.readyState, WebSocket.CLOSED)
+
+          const shouldBroadcast = pair.clientConfig.ws !== ws 
+          && pair.clientConfig.ws.readyState === WebSocket.OPEN
+          && +data.clubId === +pair.clientConfig.clubId
+
+          console.log('should broadcast', shouldBroadcast);
+
+          if (shouldBroadcast) {
+            pair.clientConfig.ws.send(message);
+          }
+        });
+  }
 };
 
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
-  keepAliveId = setInterval(() => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
-      }
-    });
-  }, 50000);
-};
+const removeAllClosedClients = () => { 
+  const closedEmployeesOnlyClients = [];
+  [...employeesOnlyMap]
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+            if (pair.clientConfig.ws.readyState === WebSocket.CLOSED) {
+              closedEmployeesOnlyClients.push(pair.cId);
+            }
+        }
+      );
 
+  closedEmployeesOnlyClients.forEach(
+    (clientId) => employeesOnlyMap.delete(clientId)
+  );
+
+  const closedTournamentClients = [];
+  [...tournamentMap]
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+            if (pair.clientConfig.ws.readyState === WebSocket.CLOSED) {
+              closedTournamentClients.push(pair.cId);
+            }
+        }
+      );
+
+      closedTournamentClients.forEach(
+    (clientId) => tournamentMap.delete(clientId)
+  );
+
+  const closedTournamentsOfClub = [];
+  [...tournamentsOfClubMap]
+        .map(([key, clientConfig]) => ({cId: key, clientConfig: clientConfig}))
+        .forEach(pair => {
+            if (pair.clientConfig.ws.readyState === WebSocket.CLOSED) {
+              closedTournamentsOfClub.push(pair.cId);
+            }
+        }
+      );
+
+      closedTournamentsOfClub.forEach(
+    (clientId) => tournamentsOfClubMap.delete(clientId)
+  );
+};
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
